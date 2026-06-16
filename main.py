3d3175aa25f869bf859456ecdbee1ba5d5b07c9f
annhthu09052006-tech <annhthu09052006@gmail.com>
2026-06-16 18:01:41 +0700

    Commit 12: Finalize entry-point link and close development phase

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-from Views.menu import ConsoleMenu
+import tkinter as tk
+from Views.menu import ManagementGUI
+
+def main():
+    # Khởi tạo cửa sổ gốc của thư viện Tkinter
+    root = tk.Tk()
+    
+    # Gắn giao diện Quản lý vào cửa sổ hệ thống
+    app = ManagementGUI(root)
+    
+    # Giữ cho cửa sổ GUI hiển thị liên tục cho đến khi nhấn dấu X thoát app
+    root.mainloop()
 
 if __name__ == "__main__":
-    app = ConsoleMenu()
-    app.show()
+    main()
