fe9a11aab0d142ddf9a94481fbe03b83d0099194
annhthu09052006-tech <annhthu09052006@gmail.com>
2026-06-16 17:16:54 +0700

    Commit 6: Implement Trainer and Schedule models

--- Models/schedule.py
+++ Models/schedule.py
@@ -1,14 +1,26 @@
 class Schedule:
     def __init__(self, schedule_id, member_id, trainer_id, time_slot):
-        self.schedule_id = schedule_id
-        self.member_id = member_id
-        self.trainer_id = trainer_id
-        self.time_slot = time_slot
+        self._schedule_id = schedule_id
+        self._member_id = member_id
+        self._trainer_id = trainer_id
+        self._time_slot = time_slot
+
+    @property
+    def schedule_id(self): return self._schedule_id
+    
+    @property
+    def member_id(self): return self._member_id
+    
+    @property
+    def trainer_id(self): return self._trainer_id
+    
+    @property
+    def time_slot(self): return self._time_slot
 
     def to_dict(self):
         return {
-            "schedule_id": self.schedule_id, 
-            "member_id": self.member_id, 
-            "trainer_id": self.trainer_id, 
+            "schedule_id": self.schedule_id,
+            "member_id": self.member_id,
+            "trainer_id": self.trainer_id,
             "time_slot": self.time_slot
         }
--- Models/trainer.py
+++ Models/trainer.py
@@ -1,8 +1,17 @@
 class Trainer:
     def __init__(self, trainer_id, name, specialty):
-        self.trainer_id = trainer_id
-        self.name = name
-        self.specialty = specialty
+        self._trainer_id = trainer_id
+        self._name = name
+        self._specialty = specialty
+
+    @property
+    def trainer_id(self): return self._trainer_id
+    
+    @property
+    def name(self): return self._name
+    
+    @property
+    def specialty(self): return self._specialty
 
     def to_dict(self):
         return {"trainer_id": self.trainer_id, "name": self.name, "specialty": self.specialty}
